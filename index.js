ec52dcf294f9236075bf7cf75090dce006d5be7e
JohanPires <johan2408.jp@gmail.com>
2024-02-19 18:40:08 +0100

    version corrigé

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const project = document.querySelector("#project-container");
 const contact = document.querySelector("#contact-container");
 const menu = document.querySelector(".menu");
 const navLink = document.querySelector("nav ul");
-// const form = document.querySelector("form");
 
 window.addEventListener("scroll", (e) => {
   if (window.scrollY > 50) {
@@ -20,15 +19,3 @@ window.addEventListener("scroll", (e) => {
 menu.addEventListener("click", () => {
   navLink.classList.toggle("activ2");
 });
-
-// const sendMail = () => {
-//   Email.send({
-//     Host: "smtp.elasticemail.com",
-//     Username: "johan2408.jp@gmail.com",
-//     Password: "DA01FBE7A320139EA2B5C8DF7CF9295E1210",
-//     To: "them@website.com",
-//     From: "you@isp.com",
-//     Subject: "This is the subject",
-//     Body: "And this is the body",
-//   }).then((message) => alert(message));
-// };
